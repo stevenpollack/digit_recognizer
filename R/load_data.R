9ec2a2120ada7f1af679fecd4aba14beb0b780f4
Steven Pollack <steven@pollackphoto.net>
2013-12-06 15:16:05 -0500

    created load_data.R script to facilitate loading of training data.
    Former-commit-id: 83744a1da402c0487ee9dc9dfb95f82eb86992a5

---
+++ R/load_data.R
@@ -0,0 +1,24 @@
+### WARNING:
+### this needs to be run from kag_digit_id/
+### ---------------------
+
+### check to see if data is loaded.
+if (!"digit.data" %in% ls()) {
+  # check to see if training_data.Rdata exists and load it
+  if (system("ls data_and_benchmarks | grep -c -e '^training_data.Rdata$'", intern=T) == "1") {
+    load(file="data_and_benchmarks/training_data.Rdata")
+  } else { # doesn't exist, so make it
+    ### check that train.csv exists
+    if (system("ls data_and_benchmarks | grep -c -e '^train.csv$'", intern=T) == "1") {
+      digit.data <- read.csv(file="data_and_benchmarks/train.csv",header=T)
+      ### condition data
+      rownames(digit.data) <- paste("image",1:42000,sep="")
+      digit.data[,1] <- as.factor(digit.data[,1])
+      
+      ### save digit.data into .Rdata file
+      save(digit.data, file="data_and_benchmarks/training_data.Rdata")
+    } else {
+      stop("Either you do not have data_and_benchmarks/train.csv or you are calling this script from somewhere other than the top-level of the project directory.")
+    }
+  }
+}
