2958e82aa90c81832f7bdd0b2ba1d9264a488404
Steven Pollack <steven@pollackphoto.net>
2013-12-21 05:58:23 -0500

    implemented selective government build which tries to make one committee per lable, with posterior likelihood greater than a desired threshold.
    Former-commit-id: 0f0c7a7408c837fd523173265bc2959a0ef0d50d

--- R/knn_based_random_gov.R
+++ R/knn_based_random_gov.R
@@ -20,7 +20,10 @@ pixel.cols <- colnames(digit.data)[which(!colnames(digit.data) %in% c("label"))]
 ### estimation of accuracy. If you want to train, you should also try and
 ### make a subset that has at least 3 samples from every class.
 ### Thus, make sure your subset is at easily bigger than 1k + 10*n + 30.
+
 set.seed(1234)
+num.of.committees <- 100
+
 subset.size <-10000 # should be greater than 1000
 subset.indices <- sample(seq.int(nrow(digit.data)), subset.size, replace=F)
 digit.subset <- digit.data[subset.indices]
@@ -31,7 +34,7 @@ test.set <- digit.subset[test.set.indices]
 training.set <- digit.subset[-test.set.indices]
 
 ### separate building set from within training sets.
-building.set.indices <- training.set[,sample(x={.I[1]:.I[.N]},size=10,replace=F),by=label][,V1]
+building.set.indices <- training.set[,sample(x={.I[1]:.I[.N]},size=num.of.committees,replace=F),by=label][,V1]
 building.set <- training.set[building.set.indices]
 training.set <- training.set[-building.set.indices]
 
@@ -42,9 +45,8 @@ test.set[,label:=NULL]
 training.set.response <- training.set[,label]
 training.set[,label:=NULL]
 
-num.of.committees <- 10
 
-buildRandomGov <- function(num.of.committees=10, digit.data) {
+buildRandomGov <- function(num.of.committees, digit.data) {
   setkey(digit.data,label)
   ### build random gov then store committees in a list
   random.house <- digit.data[,.SD[sample(x=.N, size=num.of.committees)],by=label][,`:=`(committee.num=1:num.of.committees)]
@@ -56,6 +58,55 @@ buildRandomGov <- function(num.of.committees=10, digit.data) {
   })
   return(random.gov)
 }
+
+selectivelyBuildRandomGov <- function(building.set,training.set,training.set.response,iter.limit=100,threshold=0.92,verbose=FALSE) {
+  ### consider growing a committee for the specialized purpose
+  ### of classifying label k
+  require(doRNG) # allow for reproducible foreach loop
+  set.seed(1234)
+  build.time <- system.time(committee.search.results <- foreach(class=as.factor(0:9)) %dorng% {
+    # initialize variables for while loop
+    is.trained <- FALSE
+    iter <- 0
+    best.committee <- list(likelihood=0)
+    while (iter < iter.limit) {
+      potential.committee <- building.set[,.SD[sample(x=.N, size=1)],by=label]
+      potential.committee.input <- potential.committee[,!"label",with=FALSE]
+      potential.committee.response <- potential.committee[,label]
+      predictions <- knn(train=potential.committee.input,
+                         test=training.set,
+                         cl=potential.committee.response,
+                         k=1)
+      counts <- table(predictions[predictions == training.set.response])
+      proportions <- counts / table(predictions)
+      if (proportions[class] > best.committee$likelihood) {
+        best.committee$committee <- potential.committee.input
+        best.committee$labels <- potential.committee.response
+        best.committee$likelihood <- proportions[class]
+        best.committee$metascore <- proportions
+      }
+      if (proportions[class] > threshold) {
+        is.trained <- TRUE
+        break
+      } else {
+        iter <- iter + 1
+      }
+    }
+    list(isTrained=is.trained,bestCommittee=best.committee)
+  })
+  if (verbose) {
+    cat("Random government took ", build.time[3], " seconds to build.\n")
+  }
+  if (verbose && !all(sapply(committee.search.results,function(list){list$isTrained}))) {
+    cat("Warning: some committees were unable to train beyond threshold!\n")
+  }
+  random.gov <- lapply(committee.search.results,function(list){list$bestCommittee})
+  if (verbose) {
+    cat("Maximal likelihoods: \n", paste(lapply(random.gov,function(list){round(list$likelihood,3)}),collapse=", "))
+  }
+  return(random.gov)
+}
+
 
 ### benchmark a few strategies
 ################
@@ -152,7 +203,7 @@ predictRandomGov <- function(random.gov,test.set,method="vanilla") {
   ### use kNN to block process test set and aggregate predictions
   predictions <- lapply(X=random.gov, FUN=function(rg.com) {knn(train=rg.com$committee,test=test.set,cl=rg.com$labels,k=1)}) 
   ### process results into matrix
-  predictions.mat <- matrix(unlist(predictions),nrow=10,byrow=T)
+  predictions.mat <- matrix(unlist(predictions),nrow=length(random.gov),byrow=T)
   ### process elections
   return(processElections(predictions.mat,random.gov,method))
 }
@@ -172,7 +223,54 @@ buildTrainPredict <- function(num.of.committees,building.set,training.set,traini
   list(predictions=predictions, performance=performance, randomGov=random.gov)
 }
 
-timing <- microbenchmark(out <- buildTrainPredict(num.of.committees=10,building.set,training.set,training.set.response,test.set,test.set.response,method="prob"),times=1)
+timing <- microbenchmark(out <- buildTrainPredict(num.of.committees,building.set,training.set,training.set.response,test.set,test.set.response,method="prob"),times=1)
+
+print(timing) # ~148s for 1k building set, 1k test, 8k train, 100 committees
+print(out$performance) # 46.1%
+metascore.dt <- data.table(matrix(unlist(lapply(X=out$randomGov,FUN=function(list){list$metascore})),byrow=T,ncol=10))
+best.committees.dt <- rbindlist(lapply(metascore.dt,function(col){data.table(max=max(col),row=which.max(col))}))
+
+pruned.random.gov <- out$randomGov[best.committees.dt[,row]]
+pruned.predictions <- predictRandomGov(pruned.random.gov,test.set,method=method)
+pruned.performance <- estimatePerformance(pruned.predictions,test.set.response) # 46.6%.
+
+
+out <- selectivelyBuildRandomGov(building.set,training.set,training.set.response,15,0.92,TRUE)
+
+.ignore <- foreach(x=seq.int(100)) %dopar% {
+  if (x %% 25 == 0) cat(x)
+  NULL
+}
+
+
+### sequential version that doesn't waste data:
+is.trained <- rep(FALSE,10)
+names(is.trained) <- as.factor(0:9)
+iter <- 0
+garbage <- vector("list",10)
+while (iter < 100) {
+  potential.committee <- building.set[,.SD[sample(x=.N, size=1)],by=label]
+  potential.committee.input <- potential.committee[,!"label",with=FALSE]
+  potential.committee.response <- potential.committee[,label]
+  predictions <- knn(train=potential.committee.input,
+                     test=training.set,
+                     cl=potential.committee.response,k=1)
+  counts <- table(predictions[predictions == training.set.response])
+  proportions <- counts / table(predictions)
+  
+  if (any( (proportions > 0.92) & !is.trained )) {
+      label.to.be.trained <- which.max((proportions > 0.92) & !is.trained)
+      garbage[[label.to.be.trained]] <- potential.committee
+      is.trained[label.to.be.trained] <- TRUE
+  }
+  iter <- iter+1
+}
+
+
+# ### reference ~ 252.5 s
+# system.time(ref.preds <- knn(train=training.set,test=test.set,cl=training.set.response,k=1))
+# ### accuracy ~ 95%
+# sum(diag(table(ref.preds,test.set.response)))/length(test.set.response)
 
 ### notes:
 ### it seems that training set size can quickly lead to over-fitting.
