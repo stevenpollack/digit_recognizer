1f499647a354f1eda2aa59a154ba51d53aebbb39
Steven Pollack <steven@pollackphoto.net>
2013-12-21 01:25:18 -0500

    revamped metascoring to allow for "proportions" to be used in elections. (proportions are just estimates of P(Y=k | Psi(X)=k).)
    Former-commit-id: 18344b82671f822aa2c5f505b9e09528b5c3f867

--- R/knn_based_random_gov.R
+++ R/knn_based_random_gov.R
@@ -21,12 +21,12 @@ pixel.cols <- colnames(digit.data)[which(!colnames(digit.data) %in% c("label"))]
 ### make a subset that has at least 3 samples from every class.
 ### Thus, make sure your subset is at easily bigger than 1k + 10*n + 30.
 set.seed(1234)
-subset.size <-5000 # should be greater than 1000
+subset.size <-10000 # should be greater than 1000
 subset.indices <- sample(seq.int(nrow(digit.data)), subset.size, replace=F)
 digit.subset <- digit.data[subset.indices]
 setkey(digit.subset,label)
 
-test.set.indices <- sample(seq.int(subset.size),4850,replace=F)
+test.set.indices <- sample(seq.int(subset.size),1000,replace=F)
 test.set <- digit.subset[test.set.indices]
 training.set <- digit.subset[-test.set.indices]
 
@@ -69,14 +69,29 @@ buildRandomGov <- function(num.of.committees=10, digit.data) {
 # microbenchmark(out3 <- matrix(foreach(i=seq.int(num.of.committees),.final=unlist) %do% {knn(train=random.gov[[i]]$committee,test=test.set,cl=random.gov[[i]]$labels,k=1)},nrow=10,byrow=T),times=10)
 # ##############
 
-trainRandomGov <- function(random.gov,training.set,training.set.response) {
-  ### use kNN to block-train random.government metascores (~ 126ms)
-  metascores <- lapply(X=random.gov,FUN=function(rg.com){
-    predictions <- knn(train=rg.com$committee,
-                       test=training.set,cl=rg.com$labels,k=1)
-    table(predictions[predictions == training.set.response])
-  })
+trainRandomGov <- function(random.gov,training.set,training.set.response,method="prob") {
+  ### synthetic method articially inflates counts according to how many
+  ### times a committee had the right member closest to a training example.
+  ### probability method scales the above count to yield an in-class accuracy
+  ### estimate.
   
+  ### use kNN to block-train random.government metascores (~ 126ms)
+  metascores <- if (method=="synthetic") {
+    lapply(X=random.gov,FUN=function(rg.com){
+      predictions <- knn(train=rg.com$committee,
+                         test=training.set,cl=rg.com$labels,k=1)
+      table(predictions[predictions == training.set.response])
+    }) 
+  } else if (method=="prob") {
+    lapply(X=random.gov, FUN=function(rg.com){
+      predictions <- knn(train=rg.com$committee,
+                         test=training.set,cl=rg.com$labels,k=1)
+      counts <- table(predictions[predictions == training.set.response])
+      proportions <- counts / table(predictions)
+      return(proportions) # which.max will ignore NaN's from 0/0
+    })
+  }
+   
   ### store metascore table back inside random.gov (~ 1.29 ms)
   random.gov <- lapply(X=1:num.of.committees,FUN=function(i){
     random.gov[[i]]$metascore <- metascores[[i]]
@@ -88,42 +103,58 @@ trainRandomGov <- function(random.gov,training.set,training.set.response) {
 ### help functions for predict functionality
 ########
 
-vanillaWinner <- function(votes) {
-  vote.table <- table(votes)
-  candidates <- dimnames(vote.table)$votes
+vanillaWinner <- function(predictions) {
+  vote.table <- table(predictions)
+  candidates <- dimnames(vote.table)$predictions
   winners <- candidates[which(vote.table == max(vote.table))]
   winner <- as.factor(sample(x=winners,size=1))
   return(ordered(winner,as.factor(0:9)))
 }
 
 ### synthetical bolster predictions.mat
-rescalePredictions <- function(committee.predictions,random.gov) {
-  ### random.gov[[i]]$committee's vote == committee.predictions[i]
-  synthetic.votes <- sapply(X=seq.int(length(committee.predictions)),
+rescalePredictions <- function(predictions,random.gov) {
+  ### random.gov[[i]]$committee's vote == predictions[i]
+  synthetic.votes <- sapply(X=seq.int(length(predictions)),
          FUN=function(committee.num){
-           predicted.class <- committee.predictions[committee.num]
+           predicted.class <- predictions[committee.num]
            rep(predicted.class,random.gov[[committee.num]]$metascore[predicted.class])
          })
   return(unlist(synthetic.votes))
 }
-metascoreWinner <- function(votes,random.gov) { 
-  vanillaWinner(rescalePredictions(votes,random.gov))
+syntheticMetascoreWinner <- function(predictions,random.gov) { 
+  vanillaWinner(rescalePredictions(predictions,random.gov))
 }
 
-processElections <- function(predictions.mat, random.gov){
-  vanilla.results <- apply(X=predictions.mat,MARGIN=2,FUN=vanillaWinner) 
-  metascore.results <- apply(X=predictions.mat,MARGIN=2,FUN=function(committee.predictions,random.gov){metascoreWinner(committee.predictions,random.gov)},random.gov)
-  list(vanilla=vanilla.results,metascore=metascore.results)
+### choose winner based on highest likelihood
+probMetaScoreWinner <- function(predictions,random.gov) {
+  ### random.gov[[i]]$committee's vote == committee.predictions[i]
+  candidate.certainties <- sapply(X=seq.int(length(predictions)),
+                            FUN=function(committee.num){
+                              predicted.class <- predictions[committee.num]
+                              random.gov[[committee.num]]$metascore[predicted.class]
+                            })
+  return(predictions[which.max(candidate.certainties)])
+}
+
+processElections <- function(predictions.mat, random.gov, method="vanilla"){
+  out <- if (method=="vanilla") {
+    apply(X=predictions.mat,MARGIN=2,FUN=vanillaWinner) 
+  } else if (method=="synthetic") {
+    apply(X=predictions.mat,MARGIN=2,FUN=function(committee.predictions,random.gov){syntheticMetascoreWinner(committee.predictions,random.gov)},random.gov)
+  } else if (method=="prob") {
+    apply(X=predictions.mat,MARGIN=2,FUN=function(committee.predictions,random.gov){probMetaScoreWinner(committee.predictions,random.gov)},random.gov)
+  }
+  return(out)
 }
 ########
 
-predictRandomGov <- function(random.gov,test.set) {
+predictRandomGov <- function(random.gov,test.set,method="vanilla") {
   ### use kNN to block process test set and aggregate predictions
   predictions <- lapply(X=random.gov, FUN=function(rg.com) {knn(train=rg.com$committee,test=test.set,cl=rg.com$labels,k=1)}) 
   ### process results into matrix
   predictions.mat <- matrix(unlist(predictions),nrow=10,byrow=T)
   ### process elections
-  return(processElections(predictions.mat,random.gov))
+  return(processElections(predictions.mat,random.gov,method))
 }
 
 estimatePerformance <- function(predicted.classes,actual.classes) {
@@ -133,13 +164,18 @@ estimatePerformance <- function(predicted.classes,actual.classes) {
 }
 
 ### ~ 9.65 s for 10 committees, 3.9k obs training set and 1k test set
-buildTrainPredict <- function(num.of.committees,building.set,training.set,training.set.response,test.set,test.set.response) {
+buildTrainPredict <- function(num.of.committees,building.set,training.set,training.set.response,test.set,test.set.response,method="vanilla") {
   random.gov <- buildRandomGov(num.of.committees,building.set)
-  random.gov <- trainRandomGov(random.gov,training.set,training.set.response)
-  predictions <- predictRandomGov(random.gov,test.set)
-  vanilla.performance <- estimatePerformance(predictions$vanilla,test.set.response)
-  metascore.performance <- estimatePerformance(predictions$metascore,test.set.response)
-  list(randomGov=random.gov, vanillaPerf=vanilla.performance, metascorePerf=metascore.performance)
+  random.gov <- trainRandomGov(random.gov,training.set,training.set.response,method=method)
+  predictions <- predictRandomGov(random.gov,test.set,method=method)
+  performance <- estimatePerformance(predictions,test.set.response)
+  list(predictions=predictions, performance=performance, randomGov=random.gov)
 }
 
-microbenchmark(out <- buildTrainPredict(num.of.committees=10,building.set,training.set,training.set.response,test.set,test.set.response),times=1)
+timing <- microbenchmark(out <- buildTrainPredict(num.of.committees=10,building.set,training.set,training.set.response,test.set,test.set.response,method="prob"),times=1)
+
+### notes:
+### it seems that training set size can quickly lead to over-fitting.
+### I suspect there is a direct relationship between num.of.committees and
+### accuracy when vanilla voting is implemented. Also, results seem very 
+### dependent on distance used.
